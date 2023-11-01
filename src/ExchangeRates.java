495a820d435ae7417c75f08abd7ed10eda783286
Vlad <147541054+kvs23031999@users.noreply.github.com>
2023-11-01 12:45:10 +0200

    first commit

--- src/ExchangeRates.java
+++ src/ExchangeRates.java
@@ -1,15 +1,19 @@
+import com.sun.org.apache.xml.internal.serializer.ToStream;
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 import java.util.Arrays;
 import java.util.Random;
 
 public class ExchangeRates {
     public static final int MIN_PRICE = 35;
     public static final int MAX_PRICE = 40;
+    public static String name;
 
-    public static <MinValue> void main(String[] args) {
+    public static void main(String[] args) {
         Random random = new Random();
 
         double randomValue = MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble();
-        Bank[] banks = new Bank[]{new Bank("ПРИВАТБАНК", MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble()),
+      Bank[] banks = new Bank[]{new Bank("ПРИВАТБАНК", MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble()),
                 new Bank("Сенс Банк", MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble()),
                 new Bank("Сітібанк", MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble()),
                 new Bank("ІНГ Банк Україна", MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble()),
@@ -23,21 +27,27 @@ public class ExchangeRates {
                 new Bank("Південний", MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble()),
                 new Bank("КРЕДОБАНК", MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble()),
                 new Bank("Банк Кредит Дніпро", MIN_PRICE + (MAX_PRICE - MIN_PRICE) * random.nextDouble())};
-        System.out.println(Arrays.toString(banks));
-
+        String name = banks[0].name;
+        for (int i = 0; i < banks.length; i++) {
+            System.out.println(banks[i]);
+        }
         double max = banks[0].dollarPrice;
-        for (int i = 1; i < banks.length; i++) {
-            max = Math.max(max, banks[i].dollarPrice);
-            double name = banks[0].dollarPrice;
-            for (int j = 1; j < banks.length; j++) {
-                name = Math.max(name, banks[i].dollarPrice);
+        for (int i = 0; i < banks.length; i++) {
+            if ( banks[i].dollarPrice > max ) {
+                max = banks[i].dollarPrice;
             }
-            if ( max == banks[i].dollarPrice ){
-
-            }
-            }
-            System.out.println(max);
-
+        }
+//        for (int i = 1; i < banks.length; i++) {
+//            max = Math.max(max, banks[i].dollarPrice);
+//        }
+        System.out.println(max);
+        for (int i = 0; i < banks.length; i++) {
+             if ( max == banks[i].dollarPrice ) {
+                System.out.println(banks[i]);
+             }
 
         }
     }
+
+
+}
